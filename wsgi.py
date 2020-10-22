846e70e68bb68d7570e3045bc33fa3988606c629
kirianguiller <kirianguiller@gmail.com>
2020-10-22 14:27:10 +0200

    <fix> add dotenv command in wsgy.py

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,11 @@ import os
 
 from app import create_app
 
-app = create_app(os.getenv("FLASK_ENV") or "test")
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=".flaskenv", verbose=True)
+
+env = os.getenv("FLASK_ENV") or "test"
+
+app = create_app(env)
 if __name__ == "__main__":
     app.run("0.0.0.0", 8080, debug=True)
