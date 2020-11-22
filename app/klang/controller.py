a105a6a1fe604bd91117143455985cccda431a5a
kirianguiller <kirianguiller@gmail.com>
2020-11-22 20:55:26 +0100

    <add> klang: mp3 serving view

--- app/klang/controller.py
+++ app/klang/controller.py
@@ -1,3 +1,6 @@
+import os
+
+from flask.helpers import send_file
 from app.klang.interface import TranscriptionInterface
 from app.klang.schema import TranscriptionSchema
 from flask import abort, current_app, request
@@ -15,6 +18,7 @@ class ProjectsServiceResource(Resource):
     "Klang projects"
 
     def get(self):
+        "get all projects on klang database"
         return KlangService.get_projects()
 
 
@@ -23,6 +27,7 @@ class SamplesServiceResource(Resource):
     "Klang samples (by project)"
 
     def get(self, project_name):
+        "get all samples of a project"
         return KlangService.get_project_samples(project_name)
 
 
@@ -31,6 +36,7 @@ class TimedTokensServiceResource(Resource):
     "Timed tokens"
 
     def get(self, project_name, sample_name):
+        "get the (original) timed-tokens assiociated to a conll"
         path_conll = KlangService.get_path_project_sample_conll(
             project_name, sample_name
         )
@@ -39,11 +45,12 @@ class TimedTokensServiceResource(Resource):
         return conll_audio_tokens
 
 
-@api.route("/projects/<string:project_name>/samples/<string:sample_name>/tanscriptions")
+@api.route("/projects/<string:project_name>/samples/<string:sample_name>/transcriptions")
 class TranscriptionsServiceResource(Resource):
     "Transcriptions"
 
     def get(self, project_name, sample_name):
+        "get the transcriptions of all users"
         transcriptions = TranscriptionService.load_transcriptions(
             project_name, sample_name
         )
@@ -51,9 +58,9 @@ class TranscriptionsServiceResource(Resource):
 
     @accepts(schema=TranscriptionSchema, api=api)
     def post(self, project_name, sample_name):
-        print("KK request.parsed_obj", request.parsed_obj)
+        "create/update the transcription of a user"
         if not current_user.is_authenticated:
-            abort(403, "user must be authentificated")
+            return current_app.login_manager.unauthorized()
 
         user = current_user.username
         user_trancription: TranscriptionInterface = request.parsed_obj
@@ -69,12 +76,13 @@ class TranscriptionsServiceResource(Resource):
         return user_trancription
 
 
-@api.route("/projects/<string:project_name>/samples/<string:sample_name>/tanscription/")
+@api.route("/projects/<string:project_name>/samples/<string:sample_name>/transcriptions/current-user")
 class TranscriptionUserServiceResource(Resource):
     "Transcription for one user"
 
     @responds(schema=TranscriptionSchema, api=api)
     def get(self, project_name, sample_name):
+        "get the transcription of the current_user"
         username = "__anonymous__"
         if current_user.is_authenticated:
             username = current_user.username
@@ -84,26 +92,37 @@ class TranscriptionUserServiceResource(Resource):
         transcription_user = transcriptions.get(username, {})
         return transcription_user
 
-    @accepts(schema=TranscriptionSchema, api=api)
-    def put(self, project_name, sample_name):
-        if not current_user.is_authenticated:
-            abort(403, "user must be authentificated")
+    # @accepts(schema=TranscriptionSchema, api=api)
+    # def put(self, project_name, sample_name):
+    #     if not current_user.is_authenticated:
+    #         return current_app.login_manager.unauthorized()
+    #     user = current_user.username
+    #     user_trancription: TranscriptionInterface = request.parsed_obj
 
-        user = current_user.username
-        user_trancription: TranscriptionInterface = request.parsed_obj
+    #     transcriptions = TranscriptionService.load_transcriptions(
+    #         project_name, sample_name
+    #     )
+    #     transcriptions[user] = user_trancription
 
-        transcriptions = TranscriptionService.load_transcriptions(
-            project_name, sample_name
-        )
-        transcriptions[user] = user_trancription
-
-        TranscriptionService.update_transcriptions_file(
-            project_name, sample_name, transcriptions
-        )
-        return user_trancription
+    #     TranscriptionService.update_transcriptions_file(
+    #         project_name, sample_name, transcriptions
+    #     )
+    #     return user_trancription
 
 
-# @api.route("/projects/<string:project_name>/samples/<string:sample_name>/tanscription-all")
+
+@api.route("/projects/<string:project_name>/samples/<string:sample_name>/mp3")
+class Mp3ServiceResource(Resource):
+    "MP3 Resources"
+
+    def get(self, project_name, sample_name):
+        "get the mp3 for a given sample"
+        path_project_sample = KlangService.get_path_project_sample(project_name, sample_name)
+        path_mp3 = os.path.join(path_project_sample, sample_name + ".mp3")
+        return send_file(path_mp3)
+
+
+# @api.route("/projects/<string:project_name>/samples/<string:sample_name>/transcription-all")
 # class TranscriptionServiceResource(Resource):
 #     "Transcriptions"
 
