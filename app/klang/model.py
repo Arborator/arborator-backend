01f2108fb5c8669ff1cff03a83a24e57709cc22d
kirianguiller <kirianguiller@gmail.com>
2020-11-26 13:28:13 +0100

    <change> klang: transcription model

--- app/klang/model.py
+++ app/klang/model.py
@@ -2,14 +2,13 @@ from sqlalchemy import Column, Integer, String, TEXT
 
 from app import db  # noqa
 
-class Transcription(db.Model):
-    __tablename__ = "Klang"
-    id = Column(Integer, primary_key=True)
-    user = Column(String(100), nullable=False)
-    mp3 = Column(String(100), nullable=False)
-    sound = Column(String(20), nullable=False)
-    story = Column(String(20), nullable=False)
-    accent = Column(String(20), nullable=False)
-    monodia = Column(String(20), nullable=False)
-    title = Column(String(20), nullable=False)
-    transcription = Column(TEXT)
+class Transcription():
+    def __init__(self, user, mp3, sound, story, accent, monodia, title, transcription) -> None:
+        self.user = user
+        self.mp3 = mp3
+        self.sound = sound
+        self.story = story
+        self.accent = accent
+        self.monodia = monodia
+        self.title = title
+        self.transcription = transcription
