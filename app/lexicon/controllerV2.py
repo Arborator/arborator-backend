61c7e8f104dcfc8eabe843a356d7deb6973785cb
kirianguiller <kirianguiller@gmail.com>
2021-11-04 18:59:45 +0100

    add: token processor

--- app/lexicon/controllerV2.py
+++ app/lexicon/controllerV2.py
@@ -3,6 +3,7 @@ from typing import List
 
 from app.lexicon.interfaceV2 import LexiconItemInterface
 from app.lexicon.schemaV2 import LexiconItemSchema
+from app.utils.conllup import TokenProcessor
 from app.utils.grew_utils import grew_request
 from flask_accepts.decorators.decorators import responds
 from flask_restx import Namespace, Resource, reqparse
@@ -36,7 +37,9 @@ class LexiconResource(Resource):
                 lemma=lexiconItemGrew["lemma"],
                 pos=lexiconItemGrew["POS"],
                 gloss=lexiconItemGrew["gloss"],
-                features={},
+                features=TokenProcessor.conll_mapping_to_dict_mapping(
+                    lexiconItemGrew["features"]
+                ),  # for now, grew send features as a string (conll format)
                 frequency=lexiconItemGrew["frequency"],
             )
             lexiconItems.append(lexiconItem)
