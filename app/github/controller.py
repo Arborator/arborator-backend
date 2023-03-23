902bf2a05af0ab736c0a0f8c3dac968a8687dcfb
unknown <hk_daoudi@esi.dz>
2023-03-23 16:03:22 +0100

    add commit and create new repository

--- app/github/controller.py
+++ app/github/controller.py
@@ -7,12 +7,6 @@ from flask_login import current_user
 api = Namespace("Github", description="Endpoints for dealing with github repositories") 
 
 
-
-@api.route("/<string:project_name>/me/github")
-class GithubRepository(Resource):
-    def get(self, project_name):
-        return GithubService.get_repositories(UserService.get_by_id(current_user.id).github_access_token)
-
 @api.route("/<string:project_name>/<string:username>/github-repository")
 class SynchronizedGithubRepositoryController(Resource):
     def get(self, project_name: str, username: str):
@@ -38,4 +32,51 @@ class SynchronizedGithubRepositoryController(Resource):
         GithubService.import_files_from_github(github_access_token, repository_name, project_name,user_ids)
 
         return {"status": "success"}
+    
+    def delete(self, project_name: str, username: str):
+        project = ProjectService.get_by_name(project_name)
+        ProjectService.check_if_project_exist(project)
+        user_id = UserService.get_by_username(username).id
+        GithubRepositoryService.delete_synchronization(user_id, project.id)
+        
+        return {"status": "success"}
 
+    
+@api.route("/<string:project_name>/me/github")
+class GithubRepository(Resource):
+    def get(self, project_name):
+        return GithubService.get_repositories(UserService.get_by_id(current_user.id).github_access_token)
+    
+    def post(self, project_name):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument(name="repositoryName")
+        parser.add_argument(name="description")
+        parser.add_argument(name="visibility")
+        args = parser.parse_args()
+
+        name = args.get("repositoryName")
+        description = args.get("description")
+        visibility = args.get("visibility")
+        private = True if visibility == "private" else False
+        data = {
+            "name" : name,
+            "description": description,
+            "private": private
+        }
+        github_access_token = UserService.get_by_id(current_user.id).github_access_token
+        GithubService.create_github_repository(github_access_token, data)
+        
+
+@api.route("/<string:project_name>/<string:username>/commit")
+class GithubCommit(Resource):
+    def post(self, project_name:str, username: str):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument(name="message")
+        parser.add_argument(name="repositoryName")
+        args = parser.parse_args()
+        github_message = args.get("message")
+        repository_name = args.get("repositoryName")
+        github_access_token = UserService.get_by_id(current_user.id).github_access_token
+        GithubService.commit_changes_repository(github_access_token, repository_name, project_name, github_message)
--- app/github/service.py
+++ app/github/service.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import base64
+import json
 from flask import abort
 from app import db
 from app.config import MAX_TOKENS, Config
@@ -31,10 +33,71 @@ class GithubRepositoryService:
         synchronized_github_repository = GithubRepository(**github_repository)
         db.session.add(synchronized_github_repository)
         db.session.commit()
+    
 
+    @staticmethod
+    def delete_synchronization(user_id, project_id):
+        github_repository: GithubRepository = GithubRepository.query.filter(GithubRepository.user_id == user_id).filter(GithubRepository.project_id == project_id).first()
+        db.session.delete(github_repository)
+        db.session.commit()
 
 
 class GithubService:
+
+    @staticmethod
+    def import_files_from_github(acess_token, full_name, project_name, user_ids):
+        repository_files = GithubService.get_repository_files(acess_token, full_name)
+        for file in repository_files:
+            sample_name, path_file = GithubService.create_file_from_sample_content(file.get('name'), file.get('download_url'))
+            GithubService.create_sample_github_file(sample_name, path_file, project_name, user_ids)
+
+    
+    @staticmethod 
+    def create_file_from_sample_content(file_name, download_url):
+        sample_name = file_name.split(".")[0]
+        raw_content = requests.get(download_url)
+        path_file = os.path.join(Config.UPLOAD_FOLDER, file_name)
+        file = open(path_file, "w")
+        file.write(raw_content.text)
+
+        return sample_name, path_file
+    
+
+    @staticmethod
+    def create_sample_github_file(sample_name, path_file, project_name, users_ids):
+        tokens_number = convert_users_ids(path_file, users_ids )
+        add_or_keep_timestamps(path_file)
+        if tokens_number > MAX_TOKENS:
+            abort(406, "Too big: Sample files on ArboratorGrew should have less than {max} tokens.".format(max=MAX_TOKENS))
+        GrewService.create_sample(project_name, sample_name)
+        with open(path_file, "rb") as file_to_save:
+            GrewService.save_sample(project_name, sample_name, file_to_save)
+
+    
+    @staticmethod
+    def commit_changes_repository(access_token, full_name, project_name, message):
+        files = GithubService.get_repository_files(access_token, full_name)
+        for file in files:
+            file_name =file.get('name')
+            sha = GithubService.get_file_sha(access_token, full_name, file_name)
+            file = [ file_name.split(".")[0]]
+            sample_name, sample_with_string_content = GrewService.get_samples_with_string_contents(project_name,file)
+            for sample in sample_with_string_content:
+                for  user in sample:
+                    if user != "last":
+                        content= sample[user]
+            url = "https://api.github.com/repos/{}/contents/{}".format(full_name, file_name)
+            GithubService.update_file_content(content, sha, message, url, access_token)
+
+
+
+    @staticmethod 
+    def update_file_content(content, sha, message, url, access_token):
+        encoded_content = base64.b64encode(content.encode("utf-8")).decode("utf-8")
+        data = {'sha': sha, 'message': message, 'content': encoded_content}
+        response = requests.put(url, headers = GithubService.base_header(access_token), data = json.dumps(data))
+        return response
+
 
     @staticmethod
     def base_header(access_token):
@@ -70,40 +133,30 @@ class GithubService:
     
 
     @staticmethod
-    def import_files_from_github(acess_token, full_name, project_name, user_ids):
-        repository_files = GithubService.get_repository_files(acess_token, full_name)
-        for file in repository_files:
-            sample_name, path_file = GithubService.create_file_from_file_content(file.get('name'), file.get('download_url'))
-            GithubService.create_sample_github_file(sample_name, path_file, project_name, user_ids)
-
-    @staticmethod
     def get_repository_files(access_token, full_name):
         response = requests.get("https://api.github.com/repos/{}/contents".format(full_name), headers = GithubService.base_header(access_token))
         data = response.json()
         return data
     
 
-    @staticmethod 
-    def create_file_from_file_content(file_name, download_url):
-        sample_name = file_name.split(".")[0]
-        raw_content = requests.get(download_url)
-        path_file = os.path.join(Config.UPLOAD_FOLDER, file_name)
-        file = open(path_file, "a")
-        file.write(raw_content.text)
-
-        return sample_name, path_file
+    @staticmethod
+    def get_file_sha(access_token, full_name, file_path):
+        response = requests.get("https://api.github.com/repos/{}/contents/{}".format(full_name, file_path), headers = GithubService.base_header(access_token))
+        data = response.json()
+        return data.get("sha")
     
 
     @staticmethod
-    def create_sample_github_file(sample_name, path_file, project_name, users_ids):
-        tokens_number = convert_users_ids(path_file, users_ids )
-        add_or_keep_timestamps(path_file)
-        if tokens_number > MAX_TOKENS:
-            abort(406, "Too big: Sample files on ArboratorGrew should have less than {max} tokens.".format(max=MAX_TOKENS))
-        GrewService.create_sample(project_name, sample_name)
-        with open(path_file, "rb") as file_to_save:
-            GrewService.save_sample(project_name, sample_name, file_to_save)
+    def get_list_organizations(access_token):
+        response = requests.get("https://api.github.com/user/memberships/orgs", headers = GithubService.base_header(access_token))
+        data = response.json()
+        return [ [membership.get("organization").get("login"), membership.get("organization").get("avatar_url")] for membership in data]
     
+
+    @staticmethod
+    def create_github_repository(access_token, data):
+        response = requests.post("https://api.github.com/user/repos", headers = GithubService.base_header(access_token), data = json.dumps(data) )
+        return response
 
 
 
