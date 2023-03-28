efa4ede940874c394e9cdf1709a315e9e6f43dce
kirianguiller <kirianguiller@gmail.com>
2023-03-28 15:47:55 +0200

    refactor: move parser logic inside its own "parser" module

--- app/parser/__init__.py
+++ app/parser/__init__.py
@@ -2,6 +2,6 @@ BASE_ROUTE = "parser"
 
 
 def register_routes(api, app, root="api"):
-    from .controller import api as project_api
+    from .controller import api as parser_api
 
-    api.add_namespace(project_api, path=f"/{root}/{BASE_ROUTE}")
+    api.add_namespace(parser_api, path=f"/{root}/{BASE_ROUTE}")
--- app/parser/controller.py
+++ app/parser/controller.py
@@ -1,11 +1,116 @@
-import werkzeug
+import os
+
 from app.utils.grew_utils import GrewService
-from flask import abort, current_app, request, session
-from flask_accepts.decorators.decorators import accepts, responds
-from flask_login import current_user
-from flask_restx import Namespace, Resource, reqparse
-from sqlalchemy.sql.functions import user
-from werkzeug.utils import secure_filename
+from app.config import Config
+from flask import request
+from flask_restx import Namespace, Resource
+
+from ..samples.service import add_or_keep_timestamps, add_or_replace_userid
+from ..utils.arborator_parser_utils import ArboratorParserAPI
 
 
 api = Namespace("Parser", description="Endpoints for dealing with the parser")  # noqa
+
+
+@api.route("/train/start")
+class ParserTrainStartResource(Resource):
+    def post(self):
+        params = request.get_json(force=True)
+        print("<PARSER> train/start request :", params)
+        project_name = params["project_name"]
+        train_samples_names = params["train_samples_names"]
+        train_user = params["train_user"]
+        max_epoch = params["max_epoch"]
+
+        if project_name == "undefined":
+            return {"status": "failure", "error": "NOT VALID PROJECT NAME"}
+
+        train_samples = GrewService.get_samples_with_string_contents_as_dict(project_name, train_samples_names, train_user)
+
+        return ArboratorParserAPI.train_start(project_name, train_samples, max_epoch)
+
+
+
+@api.route("/train/status")
+class ParserTrainStatusResource(Resource):
+    def post(self):
+        params = request.get_json(force=True)
+
+        print("<PARSER> parser/info request :", params)
+
+        model_info = params["model_info"]
+        project_name = model_info["project_name"]
+        model_id = model_info["model_id"]
+
+        if project_name == "undefined":
+            return {"status": "failure", "error": "NOT VALID PROJECT NAME"}
+
+        return ArboratorParserAPI.train_status(project_name, model_id)
+
+
+
+@api.route("/parse/start")
+class ParserParseStartResource(Resource):
+    def post(self):
+        params = request.get_json(force=True)
+        print("<PARSER> parse/start request :", params)
+
+        to_parse_samples_names = params["to_parse_samples_names"]
+        project_name = params["project_name"]
+        model_id = params["model_id"]
+
+        if project_name == "undefined":
+            return {"status": "failure", "error": "NOT VALID PROJECT NAME"}
+
+        to_parse_samples = GrewService.get_samples_with_string_contents_as_dict(project_name, to_parse_samples_names, "last")
+
+        return ArboratorParserAPI.parse_start(project_name, model_id, to_parse_samples)
+
+
+@api.route("/parse/status")
+class ParserParseStatus(Resource):
+    def post(self):
+        params = request.get_json(force=True)
+        print("<PARSER> parse/status request :", params)
+
+        model_id = params["model_id"]
+        project_name = params["project_name"]
+        parse_task_id = params["parse_task_id"]
+        parser_suffix = params["parser_suffix"]
+
+        if project_name == "undefined":
+            return {"status": "NOT VALID PROJECT NAME"}
+
+        parse_status_reply = ArboratorParserAPI.parse_status(parse_task_id)
+        if parse_status_reply["status"] == "failure":
+            return parse_status_reply
+
+        else:
+            data = parse_status_reply["data"]
+            if data["ready"]:
+                task_model_info = data["result"]["model_info"]
+                task_parsed_samples = data["result"]["parsed_samples"]
+
+                if task_model_info["model_id"] == model_id and task_model_info["project_name"] == project_name:
+
+                    for sample_name, sample_content in task_parsed_samples.items():
+                        path_file = os.path.join(Config.UPLOAD_FOLDER, sample_name)
+                        print('upload parsed\n', path_file)
+                        with open(path_file,'w') as f:
+                            f.write(sample_content)
+
+                        add_or_keep_timestamps(path_file, when="long_ago")
+                        add_or_replace_userid(path_file, "parser" + parser_suffix)
+                        with open(path_file, "rb") as file_to_save:
+                            print('save files')
+                            GrewService.save_sample(project_name, sample_name, file_to_save)
+
+                    return {"status": "success", "data": {"ready": True, "model_info": task_model_info}}
+            else:
+                return {"status": "success", "data": {"ready": False}}
+
+            return {"status": "failure", "error": "unknown error in ParserParseStatus"}
+
+
+
+
