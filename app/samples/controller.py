3cd2c0e160bf468193c80d2144f75380d6b2f566
kirianguiller <kirianguiller@gmail.com>
2023-05-10 10:51:10 +0200

    add: info about number of trees per user #206

--- app/samples/controller.py
+++ app/samples/controller.py
@@ -44,6 +44,7 @@ class SampleResource(Resource):
                 "number_trees": grew_sample["number_trees"],
                 "tokens": grew_sample["number_tokens"],
                 "treesFrom": grew_sample["users"],
+                "treeByUser": grew_sample["tree_by_user"],
                 "roles": {},
             }
             sample["roles"] = SampleRoleService.get_by_sample_name(
