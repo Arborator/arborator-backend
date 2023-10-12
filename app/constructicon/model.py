07556fc5e58a0442636f19ca01f99bce9dd966c6
khansadaoudi <hk_daoudi@esi.dz>
2023-10-12 16:19:58 +0200

    fix: column types of constructicon table

--- app/constructicon/model.py
+++ app/constructicon/model.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Text, ForeignKey, Integer
+from sqlalchemy import Column, Text, ForeignKey, Integer, JSON, String
 
-from sqlalchemy.dialects.postgresql import UUID, JSONB
 
 from app import db  # noqa
 
@@ -9,11 +8,11 @@ from app.shared.model import BaseM
 class Constructicon(db.Model, BaseM):
     __tablename__ = 'constructicon'
 
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(String(256), primary_key=True)
     title = Column(Text, nullable=False)
     description = Column(Text, nullable=False)
     grew_query = Column(Text, nullable=False)
-    tags = Column(JSONB, nullable=False)
+    tags = Column(JSON, nullable=False)
     project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
 
     def update(self, changes):
