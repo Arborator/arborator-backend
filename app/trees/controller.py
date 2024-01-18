7a25f5182cbbd032d9802ca886ed248b6f3ccb44
unknown <hk_daoudi@esi.dz>
2024-01-18 13:43:16 +0100

    add merge api in segmentation sentence dialog

--- app/trees/controller.py
+++ app/trees/controller.py
@@ -2,15 +2,13 @@ import os
 from flask import abort, request
 from flask_login import current_user
 from flask_restx import Namespace, Resource, reqparse
-from conllup.conllup import sentenceJsonToConll
 from conllup.processing import changeMetaFieldInSentenceConllu
 
-from app.config import Config
 from app.projects.service import LastAccessService, ProjectAccessService, ProjectService
 from app.samples.service import SampleBlindAnnotationLevelService
 from app.github.service import GithubCommitStatusService, GithubSynchronizationService
 from app.utils.grew_utils import grew_request, GrewService
-from .service import TreeService
+from .service import TreeService, TreeSegmentationService
 
 BASE_TREE = "base_tree"
 VALIDATED = "validated"
@@ -131,26 +129,33 @@ class SplitTreeResource(Resource):
         project = ProjectService.get_by_name(project_name)
         data = request.get_json()
         sent_id = data.get("sentId")
-        first_sentences = data.get("firstSents")
-        second_sentences = data.get("secondSents")
+        inserted_sentences = []
+        inserted_sentences.append(data.get("firstSents"))
+        inserted_sentences.append(data.get("secondSents"))
 
-        conll_to_insert = ''
-        for sentence_json in first_sentences.values():
-            conll_to_insert += sentenceJsonToConll(sentence_json) + '\n\n'
-        for sentence_json in second_sentences.values():
-            conll_to_insert += sentenceJsonToConll(sentence_json) + '\n\n'
-
-        file_name = sample_name + "_inserted_conll.conllu"
-        path_file = os.path.join(Config.UPLOAD_FOLDER, file_name)
-        with open(path_file, "w") as file:
-            file.write(conll_to_insert)
-
-        with open(path_file, "rb") as conll_file:
-            GrewService.insert_conll(project_name, sample_name, sent_id, conll_file)
+        TreeSegmentationService.insert_new_sentences(project_name, sample_name, sent_id, inserted_sentences)
         GrewService.eraseSentence(project_name, sample_name, sent_id)
 
         if GithubSynchronizationService.get_github_synchronized_repository(project.id):
                 GithubCommitStatusService.update(project_name, sample_name)
         LastAccessService.update_last_access_per_user_and_project(current_user.id, project_name, "write")
 
-      
+@api.route("/<string:project_name>/samples/<string:sample_name>/trees/merge")
+
+class MergeTreesResource(Resource):
+    def post(self, project_name: str, sample_name: str):
+
+        project = ProjectService.get_by_name(project_name)
+        data = request.get_json()
+        first_sent_id = data.get("firstSentId")
+        second_sent_id = data.get("secondSentId")
+        inserted_sentences = []
+        inserted_sentences.append(data.get("mergedSentences"))
+
+        TreeSegmentationService.insert_new_sentences(project_name, sample_name, first_sent_id, inserted_sentences)
+        GrewService.eraseSentence(project_name, sample_name, first_sent_id)
+        GrewService.eraseSentence(project_name, sample_name, second_sent_id)
+
+        if GithubSynchronizationService.get_github_synchronized_repository(project.id):
+                GithubCommitStatusService.update(project_name, sample_name)
+        LastAccessService.update_last_access_per_user_and_project(current_user.id, project_name, "write")
