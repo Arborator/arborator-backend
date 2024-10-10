01a46e23bf4f752b7279e68b72b5f78a28611f07
khansadaoudi <hk_daoudi@esi.dz>
2024-10-10 14:32:00 +0200

    refactor apply rule

--- app/grew/controller.py
+++ app/grew/controller.py
@@ -26,29 +26,26 @@ class ApplyRuleResource(Resource):
         
         args = request.get_json()
         data = args.get("data")
+        new_conll = ''
 
-        for sample_name in data:
-            new_conll = str()
-            sample_conll = grew_request(
-                "getConll",
-                data = {"project_id": project_name, "sample_id": sample_name}
-            )
-            sample_trees = SampleExportService.serve_sample_trees(sample_conll.get("data"))
-            for sent_id in sample_trees:
-                if sent_id in data[sample_name].keys():
-                    sample_trees[sent_id] = data[sample_name][sent_id]
-                    if ('validated' in data[sample_name][sent_id]["conlls"].keys() and 
-                        GithubRepositoryService.get_by_project_id(project.id)):
-                        GithubCommitStatusService.update_changes(project.id, sample_name)
-                for user in sample_trees[sent_id]["conlls"]:
-                    new_conll += sample_trees[sent_id]["conlls"][user] + "\n\n"
-            
+        for sample_name, sample_trees in data.items():
+            for tree in sample_trees.values():
+                
+                if 'validated' in tree["conlls"].keys() and GithubRepositoryService.get_by_project_id(project.id):
+                    GithubCommitStatusService.update_changes(project.id, sample_name)
+                    
+                for user in tree["conlls"]:
+                    new_conll += tree["conlls"][user] + "\n\n"
+                    
             file_name = sample_name + "_modified.conllu"
             path_file = os.path.join(Config.UPLOAD_FOLDER, file_name)
+            
             with open(path_file, "w") as file:
                 file.write(new_conll)
+                
             with open(path_file, "rb") as file_to_save:
                 GrewService.save_sample(project_name, sample_name, file_to_save)
+                    
         LastAccessService.update_last_access_per_user_and_project(current_user.id, project_name, "write")
 
 
