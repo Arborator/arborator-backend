3c24b0d977ba501269b1cec48af084ab3dee5e85
ramyy220 <ramyamr70@gmail.com>
2026-01-15 16:00:33 +0100

    fix(preprod): remove unnecessary configuration

--- app/utils/grew_config.py
+++ app/utils/grew_config.py
@@ -8,6 +8,6 @@ class GrewConfig:
         if env == "prod":
             self.server = "http://arborator-prod.grew.fr"
         elif env == "preprod":
-            self.server = os.getenv('EXTERNAL_PREPROD_SERVER_URL', "http://arborator-preprod.grew.fr")
+            self.server = os.getenv('EXTERNAL_PREPROD_SERVER_URL', "https://arborator-preprod.grew.fr")
         else:  # if env is dev or test
             self.server = os.getenv('EXTERNAL_SERVER_URL', "http://localhost:8222")
